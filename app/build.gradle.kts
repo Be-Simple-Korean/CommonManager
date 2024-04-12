2398be3419e6278e60416bac165ec78d380fa21c
이정현 <cw03125@openobject.net>
2024-04-12 20:56:11 +0900

    SharedPreferences 추가 - UI - Singleton Manager - Hilt Module - Provide & Singleton

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,8 +42,8 @@ android {
 }
 
 dependencies {
-    implementation("com.google.dagger:hilt-android:2.44")
-    kapt("com.google.dagger:hilt-android-compiler:2.44")
+    implementation("com.google.dagger:hilt-android:2.50")
+    kapt("com.google.dagger:hilt-android-compiler:2.50")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
--- app/src/main/java/com/example/commonmanager/fragment/MainFragment.kt
+++ app/src/main/java/com/example/commonmanager/fragment/MainFragment.kt
@@ -11,17 +11,21 @@ import com.example.commonmanager.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
 
-    private lateinit var binding:FragmentMainBinding
+    private lateinit var binding: FragmentMainBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding= FragmentMainBinding.inflate(layoutInflater, container, false);
+        binding = FragmentMainBinding.inflate(layoutInflater, container, false);
         return binding.root;
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initListeners();
+    }
+
+    private fun initListeners() {
         binding.btnPref.setOnClickListener {
             findNavController().navigate(R.id.action_mainFragment_to_prefFragment)
         }
