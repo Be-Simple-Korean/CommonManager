a608a00cc85222e372270389aff3a5bc2f2fc675
이정현 <cw03125@openobject.net>
2024-04-27 17:29:08 +0900

    AlaramManager 추가

--- app/src/main/java/com/example/commonmanager/ui/fragment/DynamicBroadFragment.kt
+++ app/src/main/java/com/example/commonmanager/ui/fragment/DynamicBroadFragment.kt
@@ -1,16 +1,27 @@
 package com.example.commonmanager.ui.fragment
 
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.SystemClock
+import android.provider.Settings
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.AlarmManagerCompat
 import androidx.core.content.ContextCompat
-import com.example.commonmanager.R
+import androidx.fragment.app.Fragment
+import com.example.commonmanager.broadcastreceiver.AlaramReceiver
 import com.example.commonmanager.broadcastreceiver.ChargeReceiver
 import com.example.commonmanager.databinding.FragmentDynamicBroadBinding
+import com.example.commonmanager.utils.getCurrentTime
+
 
 class DynamicBroadFragment : Fragment() {
 
@@ -34,11 +45,47 @@ class DynamicBroadFragment : Fragment() {
         binding.btnUnregister.setOnClickListener {
             requireActivity().unregisterReceiver(chargeReceiver)
         }
+
+        binding.btnAlarm.setOnClickListener {
+            val alaramManager =
+                requireContext().getSystemService(Context.ALARM_SERVICE) as AlarmManager
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                if (alaramManager.canScheduleExactAlarms()) {
+                    setAlaram(alaramManager)
+                } else {
+                    requireActivity().startActivity(Intent(
+                        Settings.ACTION_REQUEST_SCHEDULE_EXACT_ALARM,
+                        Uri.parse("package:${requireActivity().applicationContext.applicationInfo.packageName}")
+                    ).apply {
+                            addCategory(Intent.CATEGORY_DEFAULT)
+                            setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        })
+                }
+            } else {
+                setAlaram(alaramManager)
+            }
+
+        }
     }
 
     override fun onPause() {
         requireActivity().unregisterReceiver(chargeReceiver)
         super.onPause()
+    }
+
+    private fun setAlaram(alaramManager: AlarmManager) {
+        val intent = Intent(requireContext(), AlaramReceiver::class.java)
+        val pendingIntent =
+            PendingIntent.getBroadcast(
+                requireContext(), 0, intent,
+                PendingIntent.FLAG_IMMUTABLE
+            )
+        alaramManager.setExactAndAllowWhileIdle( // 도즈모드 대응
+            AlarmManager.ELAPSED_REALTIME_WAKEUP,
+            SystemClock.elapsedRealtime() + 5000,
+            pendingIntent
+        )
+        Log.e("set", getCurrentTime())
     }
 
     private fun registerBroadcast() {
