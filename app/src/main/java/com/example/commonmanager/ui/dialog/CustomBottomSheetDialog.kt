51f8121a6e519c818c25be82ce47f6dfbd55f82b
이정현 <cw03125@openobject.net>
2024-05-07 18:15:54 +0900

    바텀시트 다이얼로그 추가 바텀팝업 애니메이션 적용

---
+++ app/src/main/java/com/example/commonmanager/ui/dialog/CustomBottomSheetDialog.kt
@@ -0,0 +1,40 @@
+package com.example.commonmanager.ui.dialog
+
+import android.app.Dialog
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.commonmanager.databinding.ItemBottomsheetBinding
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class CustomBottomSheetDialog() : BottomSheetDialogFragment() {
+
+    private lateinit var binding: ItemBottomsheetBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = ItemBottomsheetBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        // used to show the bottom sheet dialog
+        dialog?.setOnShowListener { it ->
+            val d = it as BottomSheetDialog
+            val bottomSheet =
+                d.findViewById<View>(com.google.android.material.R.id.design_bottom_sheet)
+            bottomSheet?.let {
+                val behavior = BottomSheetBehavior.from(it)
+                behavior.state = BottomSheetBehavior.STATE_EXPANDED
+            }
+        }
+        return super.onCreateDialog(savedInstanceState)
+    }
+}
--- app/src/main/java/com/example/commonmanager/ui/fragment/DialogKtFragment.kt
+++ app/src/main/java/com/example/commonmanager/ui/fragment/DialogKtFragment.kt
@@ -6,13 +6,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.enableSavedStateHandles
 import com.example.commonmanager.R
 import com.example.commonmanager.databinding.FragmentDialogKtBinding
 import com.example.commonmanager.ui.dialog.CommonDialog
+import com.example.commonmanager.ui.dialog.CustomBottomSheetDialog
+import com.example.commonmanager.ui.dialog.SimpleBottomDialog
 
+enum class DialogType {
+    CENTER,
+    BOTTOM,
+    MODAL
+}
 
 class DialogKtFragment : Fragment() {
+
     private lateinit var binding: FragmentDialogKtBinding
+    private var dialogType = DialogType.CENTER
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -23,20 +33,93 @@ class DialogKtFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initView()
+    }
+
+    private fun initView() {
+        binding.radioGroup.setOnCheckedChangeListener { group, checkedId ->
+            when (checkedId) {
+                R.id.rb_center -> {
+                    dialogType = DialogType.CENTER
+                }
+
+                R.id.rb_bottom -> {
+                    dialogType = DialogType.BOTTOM
+                }
+
+                R.id.rb_modal -> {
+                    dialogType = DialogType.MODAL
+                }
+            }
+        }
+
         binding.btnShow.setOnClickListener {
             val title = binding.etTitle.text.toString()
             val content = binding.etValue.text.toString()
-            val dialog = CommonDialog(requireContext(), title, content, "취소", "확인",object : CommonDialog.DialogBottomClickListener{
+            when (dialogType) {
+                DialogType.CENTER -> {
+                    showCenterDialog(title, content)
+                }
+
+                DialogType.BOTTOM -> {
+                    showBottomDialog(title, content)
+                }
+
+                DialogType.MODAL -> {
+                    showModalBottomSheetDialog()
+                }
+            }
+        }
+    }
+
+    /**
+     * 중앙 팝업 노출
+     */
+    private fun showCenterDialog(title: String, content: String) {
+        val dialog = CommonDialog(
+            requireContext(),
+            title,
+            content,
+            "취소",
+            "확인",
+            object : CommonDialog.DialogBottomClickListener {
                 override fun clickLeft() {
-                    Log.e("dialog","clickLeft")
+                    Log.e("dialog", "clickLeft")
                 }
 
                 override fun clickRight() {
-                    Log.e("dialog","clickRight")
+                    Log.e("dialog", "clickRight")
                 }
             })
-            dialog.setCancelable(false)
-            dialog.show()
-        }
+        dialog.setCancelable(false)
+        dialog.show()
+    }
+
+    /**
+     * 바텀 팝업 노출
+     */
+    private fun showBottomDialog(title: String, content: String) {
+        val dialog = SimpleBottomDialog(
+            requireContext(),
+            title,
+            content,
+            "확인",
+            object : SimpleBottomDialog.BottomDialogClickListener {
+                override fun clickConfirm() {
+                    Log.e("tag", "clickConfirm()")
+                }
+
+            })
+        dialog.setCancelable(false)
+        dialog.show()
+    }
+
+    /**
+     * 바텀시트 팝업 노출
+     */
+    private fun showModalBottomSheetDialog(){
+        val dialog = CustomBottomSheetDialog()
+        dialog.isCancelable = true
+        dialog.show(requireActivity().supportFragmentManager,null)
     }
 }
